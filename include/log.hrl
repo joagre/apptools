382f6d7d37347ba5225b8b80b64e4f01c08db6da
Joakim G. <jocke@gmail.com>
2024-11-27 10:26:17 +0100

    merged fixes done in bespoke

--- include/log.hrl
+++ include/log.hrl
@@ -1,100 +1,34 @@
 -ifndef(LOG_HRL).
 -define(LOG_HRL, true).
 
--define(log_info(StringOrReport),
-        case is_map(StringOrReport) of
-            true ->
-                logger:info(
-                  maps:merge(#{module => ?MODULE,
-                               line => ?LINE,
-                               function => ?FUNCTION_NAME}, StringOrReport));
-            false ->
-                logger:info(StringOrReport)
-        end).
-
--define(log_info(FormatOrFun, Args),
-        case is_map(Args) of
-            true ->
-                logger:info(FormatOrFun,
-                            maps:merge(#{module => ?MODULE,
-                                         line => ?LINE,
-                                         function => ?FUNCTION_NAME}, Args));
-            false ->
-                logger:info(FormatOrFun, Args,
-                            #{module => ?MODULE,
-                              line => ?LINE,
-                              function => ?FUNCTION_NAME})
-        end).
-
--define(log_info(FormatOrFun, Args, Metadaya),
-        logger:info(FormatOrFun, Args,
-                    maps:merge(#{module => ?MODULE,
-                                 line => ?LINE,
-                                 function => ?FUNCTION_NAME}, MetaData))).
-
--define(log_debug(StringOrReport),
-        case is_map(StringOrReport) of
-            true ->
-                logger:debug(
-                  maps:merge(#{module => ?MODULE,
-                               line => ?LINE,
-                               function => ?FUNCTION_NAME}, StringOrReport));
-            false ->
-                logger:debug(StringOrReport)
-        end).
-
--define(log_debug(FormatOrFun, Args),
-        case is_map(Args) of
-            true ->
-                logger:debug(FormatOrFun,
-                             maps:merge(#{module => ?MODULE,
-                                          line => ?LINE,
-                                          function => ?FUNCTION_NAME}, Args));
-            false ->
-                logger:debug(FormatOrFun, Args,
-                             #{module => ?MODULE,
-                               line => ?LINE,
-                               function => ?FUNCTION_NAME})
-        end).
-
--define(log_debug(FormatOrFun, Args, Metafaya),
-        logger:debug(FormatOrFun,
-                     Args,
-                     maps:merge(#{module => ?MODULE,
+-define(log_info(Format),
+        logger:info(Format, [], #{module => ?MODULE,
                                   line => ?LINE,
-                                  function => ?FUNCTION_NAME}, MetaData))).
+                                  function => ?FUNCTION_NAME})).
 
--define(log_error(StringOrReport),
-        case is_map(StringOrReport) of
-            true ->
-                logger:error(
-                  maps:merge(#{module => ?MODULE,
-                               line => ?LINE,
-                               function => ?FUNCTION_NAME}, StringOrReport));
-            false ->
-                logger:error(StringOrReport)
-        end).
+-define(log_info(Format, Args),
+        logger:info(Format, Args, #{module => ?MODULE,
+                                    line => ?LINE,
+                                    function => ?FUNCTION_NAME})).
 
--define(log_error(FormatOrFun, Args),
-        case is_map(Args) of
-            true ->
-                logger:error(FormatOrFun,
-                             maps:merge(#{module => ?MODULE,
-                                          line => ?LINE,
-                                          function => ?FUNCTION_NAME}, Args));
-            false ->
-                logger:error(FormatOrFun, Args,
-                             #{module => ?MODULE,
-                               line => ?LINE,
-                               function => ?FUNCTION_NAME})
-        end).
+-define(log_debug(Format),
+        logger:debug(Format, [], #{module => ?MODULE,
+                                   line => ?LINE,
+                                   function => ?FUNCTION_NAME})).
 
--define(log_error(FormatOrFun, Args, Metafaya),
-        logger:error(FormatOrFun,
-                     Args,
-                     maps:merge(#{module => ?MODULE,
-                                  line => ?LINE,
-                                  function => ?FUNCTION_NAME}, MetaData))).
+-define(log_debug(Format, Args),
+        logger:debug(Format, Args, #{module => ?MODULE,
+                                     line => ?LINE,
+                                     function => ?FUNCTION_NAME})).
 
+-define(log_error(Format),
+        logger:error(Format, [], #{module => ?MODULE,
+                                   line => ?LINE,
+                                   function => ?FUNCTION_NAME})).
+
+-define(log_error(Format, Args),
+        logger:error(Format, Args, #{module => ?MODULE,
+                                     line => ?LINE,
+                                     function => ?FUNCTION_NAME})).
 
 -endif.
